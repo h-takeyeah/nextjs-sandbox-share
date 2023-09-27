1412111e9fbbe220d98fdb726a7ace6275385b23
h-takeyeah <61489178+h-takeyeah@users.noreply.github.com>
2023-09-28 02:15:10 +0900

    add react-textarea-code-editor example

--- highlight-edit-comparison/next.config.js
+++ highlight-edit-comparison/next.config.js
@@ -2,10 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  experimental: {
+    esmExternals: true,
+  }
 }
 
 const withAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.NODE_ENV === 'production',
 })
 
-module.exports = withAnalyzer(nextConfig)
+const removeImports = require('next-remove-imports')({
+  options: { },
+})
+
+module.exports = withAnalyzer(removeImports(nextConfig))
